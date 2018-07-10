b65fe72cf3308029fb5323f2d06c654aab06fbc7
SiuKeung <siu_keung@outlook.com>
2018-07-10 22:07:25 +0800

    完成了Flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -11,11 +13,18 @@ public class Flaten {
         this.array = array;
     }
 
+    // TODO: 2018-07-10 此处是否有更好的方法
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        for (Integer[] integers : array) {
+            for (Integer integer : integers) {
+                resultList.add(integer);
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return this.transformToOneDimesional().stream().distinct().collect(Collectors.toList());
     }
 }
