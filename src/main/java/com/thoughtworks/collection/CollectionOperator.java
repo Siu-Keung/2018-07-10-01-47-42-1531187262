5da7e3dada498082672fc5842514cb89d321d070
SiuKeung <siu_keung@outlook.com>
2018-07-10 21:29:03 +0800

    完成了getEvenListByIntervals(int left, int right)

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -13,7 +13,11 @@ public class CollectionOperator {
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        int increament = left > right ? -1 : 1;
+        return Stream.iterate(left, num -> num + increament)
+                .limit(Math.abs(left - right) + 1)
+                .filter(item -> item % 2 == 0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> popEvenElments(int[] array) {
