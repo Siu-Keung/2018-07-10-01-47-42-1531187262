0af35e7c4d04d50c05f590d61cf016d5d359379f
SiuKeung <siu_keung@outlook.com>
2018-07-10 22:18:46 +0800

    完成了InverseReduce

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,8 +2,10 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class InverseReduce {
 
@@ -15,7 +17,15 @@ public class InverseReduce {
         this.random = random;
     }
 
+    // TODO: 2018-07-10 是否有更好的方法？ 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        int num = number - random.nextInt(3);
+        while(num > 0){
+            resultList.add(num);
+            int temp = random.nextInt(3);
+            num -= temp;
+        }
+        return resultList;
     }
 }
